34d0b84e08e6c204d11861d0779b3c3512c166d4
Michael Rook <michael@handmadeweb.com.au>
2021-03-11 11:08:49 +1100

    Change loading order.

--- classes/DB.php
+++ classes/DB.php
@@ -4,4 +4,6 @@ if (! class_exists('DB')) {
     class DB extends \HandmadeWeb\Illuminate\DB
     {
     }
+
+    class_exists('DB') ? new \DB : null;
 }
--- src/Loader.php
+++ src/Loader.php
@@ -6,7 +6,6 @@ class Loader
 {
     public static function boot()
     {
-        class_exists('DB') ? new \DB : null;
         class_exists('View') ? new \View : null;
     }
 }
